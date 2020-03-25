bf7e2e1a412e195f3b66f6d3aeea43cb320f4cd5
Robson Duarte <robson.o.d@gmail.com>
2020-03-24 21:05:53 -0300

    add some widegts

--- flutter-curse/flutter_basic/lib/main.dart
+++ flutter-curse/flutter_basic/lib/main.dart
@@ -5,11 +5,32 @@ main() => runApp(QuestionApp());
 class QuestionApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'How is your favorite color ?',
+      'How is your favorite pet ?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Questions'),
-        ),body: Text("Hello Ontrcker on Flutter"),
+        ),body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            RaisedButton(
+              child: Text('Anwser 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Anwser 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Anwser 1'),
+              onPressed: null,
+            ),
+          ],
+        ),
       )
     );
   }
