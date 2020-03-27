1fe65babde2adfee119a7f68fff7936aee5005a4
Robson Duarte <robson.o.d@gmail.com>
2020-03-26 22:24:08 -0300

    using imperative programming

--- flutter-curse/flutter_basic/lib/main.dart
+++ flutter-curse/flutter_basic/lib/main.dart
@@ -17,32 +17,30 @@ class _QuestionAppState extends State<QuestionApp> {
   Widget build(BuildContext context) {
     final questionsAndAnswers = [
       {
-        'text': 'How is your favorite color ?',
-        'awsers': ['Black', 'Red', 'Green', 'White']
+        'question': 'How is your favorite color ?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
       },
       {
-        'text': 'How is your favorite color ?',
-        'awsers': ['Dog', 'Cat', 'Duck', 'Elefant']
+        'question': 'How is your favorite color ?',
+        'answers': ['Dog', 'Cat', 'Duck', 'Elefant'],
       },
       {
-        'text': 'How is your favorite Theacher ?',
-        'awsers': ['Robson', 'Ana Mara', 'Cida', 'Antonio']
+        'question': 'How is your favorite Theacher ?',
+        'answers': ['Robson', 'Ana Mara', 'Cida', 'Antonio'],
       }
     ];
+
+    List<String> answers = questionsAndAnswers[_questionSelected]['answers'];
 
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('Questions'),
       ),
-      body: Column(
-        children: [
-          Question(questionsAndAnswers[_questionSelected]['text']),
-          Answer('Anwser 1', _answer),
-          Answer('Anwser 2', _answer),
-          Answer('Anwser 3', _answer),
-        ],
-      ),
+      body: Column(children: [
+        Question(questionsAndAnswers[_questionSelected]['question']),
+        ...answers.map((answer) => Answer(answer, _answer)).toList(),
+      ]),
     ));
   }
 }
