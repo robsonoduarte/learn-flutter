bb29a2002019e0bb6a8b47a805b4287c744f32b8
Robson Duarte <robson.o.d@gmail.com>
2020-03-31 22:55:38 -0300

    fix the bug of count answer

--- flutter-curse/flutter_basic/lib/main.dart
+++ flutter-curse/flutter_basic/lib/main.dart
@@ -6,6 +6,20 @@ main() => runApp(QuestionApp());
 
 class _QuestionAppState extends State<QuestionApp> {
   var _questionSelected = 0;
+  final _questionsAndAnswers = const [
+    {
+      'question': 'How is your favorite color ?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'question': 'How is your favorite color ?',
+      'answers': ['Dog', 'Cat', 'Duck', 'Elefant'],
+    },
+    {
+      'question': 'How is your favorite Theacher ?',
+      'answers': ['Robson', 'Ana Mara', 'Cida', 'Antonio'],
+    }
+  ];
 
   void _answer() {
     setState(() {
@@ -13,34 +27,27 @@ class _QuestionAppState extends State<QuestionApp> {
     });
   }
 
+  _hasQuestionSelected() {
+    return _questionSelected < _questionsAndAnswers.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final questionsAndAnswers = [
-      {
-        'question': 'How is your favorite color ?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'question': 'How is your favorite color ?',
-        'answers': ['Dog', 'Cat', 'Duck', 'Elefant'],
-      },
-      {
-        'question': 'How is your favorite Theacher ?',
-        'answers': ['Robson', 'Ana Mara', 'Cida', 'Antonio'],
-      }
-    ];
-
-    List<String> answers = questionsAndAnswers[_questionSelected]['answers'];
+    List<String> answers = _hasQuestionSelected()
+        ? _questionsAndAnswers[_questionSelected]['answers']
+        : null;
 
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Questions'),
-      ),
-      body: Column(children: [
-        Question(questionsAndAnswers[_questionSelected]['question']),
-        ...answers.map((answer) => Answer(answer, _answer)).toList(),
-      ]),
+          appBar: AppBar(
+            title: Text('Questions'),
+          ),
+          body: _hasQuestionSelected()
+              ? Column(children: [
+                  Question(_questionsAndAnswers[_questionSelected]['question']),
+                  ...answers.map((answer) => Answer(answer, _answer)).toList(),
+                ])
+              : null,
     ));
   }
 }
