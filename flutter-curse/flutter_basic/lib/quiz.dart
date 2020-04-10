50598504ae37b147cd94417769256e8df195c989
Robson Duarte <robson.o.d@gmail.com>
2020-04-10 00:27:51 -0300

    refactor the project

--- flutter-curse/flutter_basic/lib/quiz.dart
+++ flutter-curse/flutter_basic/lib/quiz.dart
@@ -4,31 +4,13 @@ import 'package:flutter_basic/question.dart';
 import 'answer.dart';
 
 class Quiz extends StatelessWidget {
+  final Question question;
+  final List<Answer> answers;
 
-  final List<Map<String, Object>> questionsAndAnswers;
-  final int questionSelected;
-  final void Function() answer;
-
-  const Quiz({
-    @required this.questionsAndAnswers,
-    @required this.questionSelected,
-    @required this.answer
-  });
-
-  hasQuestionSelected() {
-    return questionSelected < questionsAndAnswers.length;
-  }
+  const Quiz(this.question, this.answers);
 
   @override
   Widget build(BuildContext context) {
-    List<String> answers = hasQuestionSelected()
-        ? questionsAndAnswers[questionSelected]['answers']
-        : null;
-    return Column(children: [
-    Question(questionsAndAnswers[questionSelected]['question']),
-    ...answers.map((asw) => Answer(asw, answer)).toList(),
-    ]
-    );
+    return Column(children: [question, ...answers]);
   }
-
 }
