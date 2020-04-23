df86fd4643787160cf449b231f3cffa426f280ca
Robson Duarte <robson.o.d@gmail.com>
2020-04-22 22:36:25 -0300

    starting the transaction model feature

--- flutter_course/expenses/lib/main.dart
+++ flutter_course/expenses/lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:expenses/model/model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 main() => runApp(ExpensesApp());
 
-class ExpensesApp extends StatelessWidget{
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +13,35 @@ class ExpensesApp extends StatelessWidget{
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(id: '1', title: 'Beer', value: 8.5, date: DateTime.now()),
+    Transaction(id: '2', title: 'Sofa', value: 2.0, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Personal Expenses'),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Graphic'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Transaction List'),
+          )
+        ],
+      ),
     );
   }
-
 }
