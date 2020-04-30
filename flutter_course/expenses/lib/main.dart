262f02dc56d51cd0890d5ad983dba032fce65b99
Robson Duarte <robson.o.d@gmail.com>
2020-04-29 22:59:39 -0300

    Style Transaction Widget (#8)

--- flutter_course/expenses/lib/main.dart
+++ flutter_course/expenses/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:expenses/model/model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -43,12 +44,40 @@ class HomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
-                      child: Text(tr.value.toString()),
+                      child: Text(
+                          tr.value.toString(),
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple
+                        ),
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2
+                        )
+                      ),
+                      padding: EdgeInsets.all(10),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tr.title),
-                        Text(tr.date.toString())
+                        Text(tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey
+                          ),
+                        )
                       ],
                     )
                   ],
