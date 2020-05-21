2b4054f9a0461cf733212c0d6a407b69b6132c00
Robson Duarte <robson.o.d@gmail.com>
2020-05-21 20:04:49 -0300

    fix stateful widegt

--- flutter_course/expenses/lib/components/transaction_form.dart
+++ flutter_course/expenses/lib/components/transaction_form.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget{
-
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
 
-  _onSubmit(){
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
+  _onSubmit() {
     final title = titleController.text;
     final value = double.tryParse(valueController.text) ?? 0.0;
 
-    if(title.isEmpty || value <= 0){
+    if (title.isEmpty || value <= 0) {
       return;
     }
 
-    onSubmit(title,value);
+    widget.onSubmit(title, value);
   }
 
   @override
   Widget build(BuildContext context) {
-    return           Card(
+    return Card(
       elevation: 5,
       child: Padding(
         padding: const EdgeInsets.all(10),
@@ -54,8 +58,6 @@ class TransactionForm extends StatelessWidget{
           ],
         ),
       ),
-    )
-    ;
+    );
   }
-
 }
--- flutter_course/expenses/lib/main.dart
+++ flutter_course/expenses/lib/main.dart
@@ -32,7 +32,7 @@ class _HomePageState extends State<HomePage> {
     showModalBottomSheet(
         context: context,
         builder: (_) {
-          return TransactionForm(null);
+          return TransactionForm(_addTransaction);
         });
   }
 
