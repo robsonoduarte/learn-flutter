81c00dc1e644533bffd224f06f93de946a13b0eb
Robson Duarte <robson.o.d@gmail.com>
2020-08-27 20:54:08 -0300

    HTTP Post and Future (#26)

--- flutter_course/shop/lib/providers/products.dart
+++ flutter_course/shop/lib/providers/products.dart
@@ -1,14 +1,15 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import './product.dart';
 import '../data/dummy_data.dart';
 
 class Products with ChangeNotifier {
-
   List<Product> _items = DUMMY_PRODUCTS;
 
-  List<Product> get items  => [ ..._items ];
+  List<Product> get items => [..._items];
 
   int get itemsCount {
     return _items.length;
@@ -18,24 +19,38 @@ class Products with ChangeNotifier {
     return _items.where((prod) => prod.isFavorite).toList();
   }
 
-  void addProduct(Product newProduct) {
-    _items.add(Product(
-      id: Random().nextDouble().toString(),
-      title: newProduct.title,
-      description: newProduct.description,
-      price: newProduct.price,
-      imageUrl: newProduct.imageUrl
-    ));
-    notifyListeners();
+  Future<void> addProduct(Product newProduct) {
+    return http
+        .post(
+      "https://flutter-curse.firebaseio.com/products.json",
+      body: json.encode(
+        {
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'price': newProduct.price,
+          'imageUrl': newProduct.imageUrl,
+          'isFavorite': newProduct.isFavorite,
+        },
+      ),
+    )
+        .then((response) {
+      _items.add(Product(
+          id: json.decode(response.body)['name'],
+          title: newProduct.title,
+          description: newProduct.description,
+          price: newProduct.price,
+          imageUrl: newProduct.imageUrl));
+      notifyListeners();
+    });
   }
 
   void updateProduct(Product product) {
-    if(product == null || product.id == null) {
+    if (product == null || product.id == null) {
       return;
     }
 
     final index = _items.indexWhere((prod) => prod.id == product.id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
@@ -43,21 +58,21 @@ class Products with ChangeNotifier {
 
   void deleteProduct(String id) {
     final index = _items.indexWhere((prod) => prod.id == id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items.removeWhere((prod) => prod.id == id);
       notifyListeners();
     }
   }
 }
 
-  // bool _showFavoriteOnly = false;
-  
-  // void showFavoriteOnly() {
-  //   _showFavoriteOnly = true;
-  //   notifyListeners();
-  
-  // }
-  // void showAll() {
-  //   _showFavoriteOnly = false;
-  //   notifyListeners();
-  // }
+// bool _showFavoriteOnly = false;
+
+// void showFavoriteOnly() {
+//   _showFavoriteOnly = true;
+//   notifyListeners();
+
+// }
+// void showAll() {
+//   _showFavoriteOnly = false;
+//   notifyListeners();
+// }
