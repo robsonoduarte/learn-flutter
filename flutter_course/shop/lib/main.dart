fe7ca9ec51e26ba20f9857964a59bec0caaa26d0
Robson Duarte <robson.o.d@gmail.com>
2021-01-12 19:59:50 -0300

    Login Screen (#29)

--- flutter_course/shop/lib/main.dart
+++ flutter_course/shop/lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/views/auth_screen.dart';
 
-import './utils/app_routes.dart';
-
-import './views/products_overview_screen.dart';
-import './views/product_detail_screen.dart';
-import './views/cart_screen.dart';
-import './views/orders_screen.dart';
-import './views/products_screen.dart';
-import './views/product_form_screen.dart';
-
-import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/products.dart';
+import './utils/app_routes.dart';
+import './views/cart_screen.dart';
+import './views/orders_screen.dart';
+import './views/product_detail_screen.dart';
+import './views/product_form_screen.dart';
+import './views/products_overview_screen.dart';
+import './views/products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,6 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: ProductOverviewScreen(),
         routes: {
+          AppRoutes.AUTH: (ctx) => AuthScreen(),
           AppRoutes.HOME: (ctx) => ProductOverviewScreen(),
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
           AppRoutes.CART: (ctx) => CartScreen(),
