5f157c70200985b59ecfc9be508fe2c7c36bece9
Robson Duarte <robosn.o.d@gmail.com>
2022-08-24 22:41:02 -0300

    :clap:

---
+++ flutter_course/meals/lib/screens/categories_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+      maxCrossAxisExtent: 200,
+      childAspectRatio: 1.5,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+    ),
+    children: [
+
+    ],);
+  }
+}
