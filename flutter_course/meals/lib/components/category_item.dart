f8c2577802a30ba45935a234fac909d80e6ada4f
Robson Duarte <robson.o.d@gmail.com>
2022-08-25 22:39:56 -0300

    starting the app to study the flutter navigation (#39)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

---
+++ flutter_course/meals/lib/components/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final Category category;
+
+  const CategoryItem({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.5),
+          category.color,
+        ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(category.title),
+    );
+  }
+}
---
+++ flutter_course/meals/lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Vamos Cozinhar"),
+      ),
+      body: GridView(gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: dummyCategories.map((cat){
+        return CategoryItem(category: cat);
+      }).toList(),),
+    );
+  }
+}
