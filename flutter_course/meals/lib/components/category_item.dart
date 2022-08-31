df26696af0bcfa1b8482519e7573fcf21f69581c
Robson Duarte <robosn.o.d@gmail.com>
2022-08-30 23:01:20 -0300

    class 184/185/186/187

--- flutter_course/meals/lib/components/category_item.dart
+++ flutter_course/meals/lib/components/category_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final Category category;
 
   const CategoryItem({super.key, required this.category});
@@ -13,15 +12,19 @@ class CategoryItem extends StatelessWidget {
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(colors: [
-          category.color.withOpacity(0.5),
-          category.color,
-        ],
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
-      child: Text(category.title),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
     );
   }
 }
