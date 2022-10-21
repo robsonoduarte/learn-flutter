44dac4d9ca24de88ff6810cb7acb6afbc68e9391
Robson Duarte <robson.o.d@gmail.com>
2022-10-20 22:38:46 -0300

    class week 3 out (#44)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

--- flutter_course/meals/lib/main.dart
+++ flutter_course/meals/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/models/settings.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/settings_screen.dart';
@@ -14,7 +16,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+
   var _avaibleMeals = dummyMeals;
+
+  List<Meal> _favoritesMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+      _avaibleMeals = dummyMeals.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoritesMeals.contains(meal)
+          ? _favoritesMeals.remove(meal)
+          : _favoritesMeals.add(meal);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +65,11 @@ class _MyAppState extends State<MyApp> {
             ),
       ),
       routes: {
-        AppRoutes.home: (context) => TabsScreen(),
+        AppRoutes.home: (context) => TabsScreen(_favoritesMeals),
         AppRoutes.categoriesMeals: (context) =>
             CategoriesMealsScreen(_avaibleMeals),
-        AppRoutes.mealDetail: (context) => MealDatailScreen(),
-        AppRoutes.settings: (context) => SettingsScreen(),
+        AppRoutes.mealDetail: (context) => MealDetailScreen(_toggleFavorite),
+        AppRoutes.settings: (context) => SettingsScreen(settings, _filterMeals),
       },
     );
   }
--- flutter_course/meals/lib/screens/favorite_screen.dart
+++ flutter_course/meals/lib/screens/favorite_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+
+import '../models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoriteScreen(this.favoriteMeals);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Minhas Refeições Favorita',
-      ),
-    );
+    if (favoriteMeals.isNotEmpty) {
+      return const Center(
+        child: Text(
+          'Nenhuma refeição foi marcada com favorita',
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (context, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
