66556b6a28ec17dbafd985b98f4fdbc75d7e3384
Robson Duarte <robosn.o.d@gmail.com>
2023-01-12 23:30:28 -0300

    class week 2 jan

--- flutter_course/shop_two/lib/main.dart
+++ flutter_course/shop_two/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_two/models/product_list.dart';
 import 'package:shop_two/pages/product_detail_page.dart';
 import 'package:shop_two/pages/products_overview_page.dart';
 import 'package:shop_two/utils/app_routes.dart';
@@ -13,18 +15,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (context) => const ProductDetailPage()
+        },
       ),
-      home: ProductsOverviewPage(),
-      routes: {AppRoutes.productDetail: (context) => const ProductDetailPage()},
     );
   }
 }
