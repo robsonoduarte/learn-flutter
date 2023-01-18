392fff2e56c01bf7d8572492539f18bb0f54b5d4
Robson Duarte <robosn.o.d@gmail.com>
2023-01-17 23:10:00 -0300

    class week 3 jan

--- flutter_course/shop_two/lib/pages/products_overview_page.dart
+++ flutter_course/shop_two/lib/pages/products_overview_page.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop_two/components/product_item.dart';
-import 'package:shop_two/models/product.dart';
-import 'package:shop_two/models/product_list.dart';
+
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({Key? key}) : super(key: key);
-
+  const ProductsOverviewPage({super.key});
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (context, index) {
-          return ProductItem(product: loadedProducts[index]);
-        },
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
