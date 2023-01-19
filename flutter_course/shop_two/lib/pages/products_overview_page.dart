f99c5b01495a6b5f986b8bd46af71bdee7a4321a
Robson Duarte <robosn.o.d@gmail.com>
2023-01-18 23:26:00 -0300

    class week 3 jan

--- flutter_course/shop_two/lib/pages/products_overview_page.dart
+++ flutter_course/shop_two/lib/pages/products_overview_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_two/models/product_list.dart';
 
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  favorites,
+  all;
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
@@ -9,6 +16,28 @@ class ProductsOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorites,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              )
+            ],
+            onSelected: (FilterOptions option) {
+              if (option == FilterOptions.favorites) {
+                context.read<ProductList>().showFavoriteOnly();
+              } else {
+                context.read<ProductList>().showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
