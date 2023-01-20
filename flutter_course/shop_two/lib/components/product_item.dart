3d14e197a69420c78451c35fbe907f73d6ddb16e
Robson Duarte <robson.o.d@gmail.com>
2023-01-19 23:37:55 -0300

    class week 3 jan (#49)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

--- flutter_course/shop_two/lib/components/product_item.dart
+++ flutter_course/shop_two/lib/components/product_item.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_two/models/product.dart';
 import 'package:shop_two/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-  const ProductItem({Key? key, required this.product}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -24,11 +26,15 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.secondary,
             ),
           ),
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (context, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
           ),
         ),
--- flutter_course/shop_two/lib/pages/products_overview_page.dart
+++ flutter_course/shop_two/lib/pages/products_overview_page.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_two/components/product_item.dart';
-import 'package:shop_two/models/product.dart';
 import 'package:shop_two/models/product_list.dart';
 
-class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({Key? key}) : super(key: key);
+import '../components/product_grid.dart';
 
+enum FilterOptions {
+  favorites,
+  all;
+}
+
+class ProductsOverviewPage extends StatelessWidget {
+  const ProductsOverviewPage({super.key});
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorites,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              )
+            ],
+            onSelected: (FilterOptions option) {
+              if (option == FilterOptions.favorites) {
+                context.read<ProductList>().showFavoriteOnly();
+              } else {
+                context.read<ProductList>().showAll();
+              }
+            },
+          ),
+        ],
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (context, index) {
-          return ProductItem(product: loadedProducts[index]);
-        },
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
