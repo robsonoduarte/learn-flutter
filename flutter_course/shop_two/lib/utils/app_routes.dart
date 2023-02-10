07129e190f9dc1d1ce5b7be2d7e99ad30829c856
Robson Duarte <robosn.o.d@gmail.com>
2023-02-10 18:58:53 -0300

    :pray:

--- flutter_course/shop_two/lib/utils/app_routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const home = '/';
-  static const productDetail = '/product-detail';
-  static const cart = '/cart';
-  static const orders = '/orders';
-  static const products = '/products';
-}
