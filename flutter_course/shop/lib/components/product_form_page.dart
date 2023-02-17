3358c40b82323a734320692fbea5127105acf9fa
Robson Duarte <robson.o.d@gmail.com>
2023-02-16 23:52:42 -0300

    class of fev week 3 (#59)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

---
+++ flutter_course/shop/lib/components/product_form_page.dart
@@ -0,0 +1,171 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductFormPage extends StatefulWidget {
+  const ProductFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+  final _urlImageFocus = FocusNode();
+  final _urlImageController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
+
+  @override
+  void initState() {
+    super.initState();
+    _urlImageFocus.addListener(_updateImage);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+    _urlImageFocus.dispose();
+    _urlImageController.removeListener(_updateImage);
+  }
+
+  void _updateImage() {
+    setState(() {});
+  }
+
+  void _submit() {
+    var isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState?.save();
+    final product = Product(
+      id: Random().nextDouble().toString(),
+      name: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+        actions: [
+          IconButton(
+              onPressed: _submit,
+              icon: const Icon(
+                Icons.save,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  labelText: 'name',
+                ),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
+                onSaved: (value) {
+                  _formData['name'] = value ?? '';
+                },
+                validator: (value) {
+                  final _value = value ?? '';
+                  if (_value.trim().isEmpty) {
+                    return 'Name is required';
+                  }
+                  if (_value.trim().length < 3) {
+                    return 'Name must be three or more characters';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocus,
+                decoration: const InputDecoration(
+                  labelText: 'price',
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
+                onSaved: (value) {
+                  _formData['price'] = double.parse(value ?? '0');
+                },
+              ),
+              TextFormField(
+                focusNode: _descriptionFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  labelText: 'description',
+                ),
+                onSaved: (value) {
+                  _formData['description'] = value ?? '';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      focusNode: _urlImageFocus,
+                      controller: _urlImageController,
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      decoration: const InputDecoration(
+                        labelText: 'url of image',
+                      ),
+                      onFieldSubmitted: (_) => _submit(),
+                      onSaved: (value) {
+                        _formData['imageUrl'] = value ?? '';
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    child: _urlImageController.text.isEmpty
+                        ? const Text('get the url')
+                        : FittedBox(
+                            fit: BoxFit.cover,
+                            child: Image.network(_urlImageController.text),
+                          ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
