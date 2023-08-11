5f1d3ac20e9bd6b7e3f7f71c7e35468ae080c1ca
Robson Duarte <robosn.o.d@gmail.com>
2023-08-10 23:11:22 -0300

    starting chapter 14 chat app

---
+++ flutter_course/chat/lib/components/auth_form.dart
@@ -0,0 +1,103 @@
+import 'package:chat/models/auth_form_data.dart';
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+  const AuthForm({super.key, required this.onSubmit});
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _authFormData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate() ?? false;
+    if (!isValid) return;
+    widget.onSubmit(_authFormData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              if (_authFormData.isSignup)
+                TextFormField(
+                  key: const ValueKey('name'),
+                  initialValue: _authFormData.name,
+                  onChanged: (name) => _authFormData.name = name,
+                  decoration: const InputDecoration(
+                    labelText: 'name',
+                  ),
+                  validator: (value) {
+                    final name = value ?? '';
+                    if (name.trim().length < 5) {
+                      return 'name must be a least 5 characters';
+                    }
+                    return null;
+                  },
+                ),
+              TextFormField(
+                key: const ValueKey('email'),
+                initialValue: _authFormData.email,
+                onChanged: (email) => _authFormData.email = email,
+                decoration: const InputDecoration(
+                  labelText: 'e-mail',
+                ),
+                validator: (value) {
+                  final email = value ?? '';
+                  if (!email.contains('@')) {
+                    return 'invalid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                key: const ValueKey('password'),
+                initialValue: _authFormData.password,
+                onChanged: (password) => _authFormData.password = password,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'password',
+                ),
+                validator: (value) {
+                  final password = value ?? '';
+                  if (password.length < 5) {
+                    return 'password must be a least 6 characters';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                onPressed: _submit,
+                child: Text(_authFormData.isLogin ? 'login' : 'register'),
+              ),
+              TextButton(
+                child: Text(
+                  _authFormData.isLogin
+                      ? 'create a new account ?'
+                      : 'already have account ?',
+                ),
+                onPressed: () {
+                  setState(() {
+                    _authFormData.toggleAuthMode();
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
