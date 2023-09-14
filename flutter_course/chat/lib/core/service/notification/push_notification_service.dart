527f7fe9a180b255284b9d997c4fcaea49fa60fa
Robson Duarte <robosn.o.d@gmail.com>
2023-09-14 00:24:20 -0300

    add firebase push notification

--- flutter_course/chat/lib/core/service/notification/push_notification_service.dart
+++ flutter_course/chat/lib/core/service/notification/push_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:chat/core/models/chat_notification.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 class ChatNotificationService with ChangeNotifier {
@@ -14,5 +15,26 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int index) {
     _items.removeAt(index);
     notifyListeners();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen((message) {
+        add(ChatNotification(
+          message.notification!.title ?? 'Not Informed',
+          message.notification!.body ?? 'Not Informed',
+        ));
+      });
+    }
   }
 }
