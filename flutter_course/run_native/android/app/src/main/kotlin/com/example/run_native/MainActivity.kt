c5b1f67985b17f72598cf34fbe3ee29857920e01
Robson Duarte <robosn.o.d@gmail.com>
2023-09-19 23:40:20 -0300

    :done:

--- flutter_course/run_native/android/app/src/main/kotlin/com/example/run_native/MainActivity.kt
+++ flutter_course/run_native/android/app/src/main/kotlin/com/example/run_native/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.example.run_native
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "dev.bravox/native"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler{
+            call, result ->
+            if(call.method == "calcSum"){
+                var a = call.argument<Int>("a")?.toInt() ?: 0
+                var b = call.argument<Int>("b")?.toInt() ?: 0
+                result.success(a + b)
+            }else{
+                result.notImplemented()
+            }
+        }
+    }
 }
--- flutter_course/run_native/ios/Runner/AppDelegate.swift
+++ flutter_course/run_native/ios/Runner/AppDelegate.swift
@@ -7,6 +7,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let nativeChannel = FlutterMethodChannel(name: "dev.bravox/native", binaryMessenger: controller.binaryMessenger)
+      nativeChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+          guard call.method == "calcSum" else {
+              result(FlutterMethodNotImplemented)
+              return
+          }
+          if let args = call.arguments as? [String: Any],
+             let a = args["a"] as? Int,
+             let b = args["b"] as? Int {
+              result(a + b)
+          } else {
+              result(-1)
+          }
+      })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
