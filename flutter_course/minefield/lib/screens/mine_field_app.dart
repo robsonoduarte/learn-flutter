bd51c653932292a1cda6e7c8d7f49448ddded860
robsonoduarte <robosn.o.d@gmail.com>
2023-09-28 00:09:08 -0300

    finish the app minefield

--- flutter_course/minefield/lib/screens/mine_field_app.dart
+++ flutter_course/minefield/lib/screens/mine_field_app.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:minefield/components/field_widget.dart';
+import 'package:minefield/components/result_widget.dart';
+import 'package:minefield/models/explosion_exception.dart';
+import 'package:minefield/models/field.dart';
 
 class MineFieldApp extends StatelessWidget {
   const MineFieldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Field n1 = Field(1, 0);
+    n1.undermined();
+    Field n2 = Field(1, 0);
+    n2.undermined();
+    Field field = Field(0, 0);
+    field.addNeighborhood(n1);
+    field.addNeighborhood(n2);
+    try {
+      //field.undermined();
+      //field.open();
+      field.toggleMarking();
+    } on ExplosionException {}
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Minefield'),
+        appBar: ResultWidget(
+          won: true,
+          onRestart: _restart,
         ),
         body: Container(
-          child: Text('Board'),
+          child: FieldWidget(
+            field: field,
+            onOpen: _open,
+            onToggleMarking: _toggleMarking,
+          ),
         ),
       ),
     );
+  }
+
+  _restart() {
+    print('restaring');
+  }
+
+  _open(Field field) {
+    print("open");
+  }
+
+  _toggleMarking(Field field) {
+    print("toggle");
   }
 }
