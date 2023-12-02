f21ea8e39553a1064aa50b65f8a55bcbd91f3113
Robson Duarte <robson.o.d@gmail.com>
2023-12-01 23:24:57 -0300

    Dart control flow (#110)

---
+++ flutter_course/dart_control_flow/lib/loop.dart
@@ -0,0 +1,50 @@
+main() {
+  // Loops
+  // https://dart.dev/language/loops
+
+  // FOR
+  // https://dart.dev/language/loops#for-loops
+  var message = StringBuffer('Dart is fun');
+  for (var i = 0; i < 5; i++) {
+    message.write('!');
+  }
+  print(message);
+
+  // Closures inside of Dart’s for loops capture the value of the index
+  var callbacks = [];
+  for (var i = 0; i < 2; i++) {
+    callbacks.add(() => print(i));
+  }
+
+  for (final c in callbacks) {
+    c();
+  }
+
+  // fon in
+  const candidates = const ['Ana Mara', 'Robson', 'Antonio'];
+  for (final candidate in candidates) {
+    print('the candidate is $candidate');
+  }
+
+  // pattern in a for in
+  final people = [
+    Person('Robson', 47, 123),
+    Person('Ana Mara', 48, 345),
+    Person('Antonio', 73, 567),
+  ];
+  for (final Person(:name, :age) in people) {
+    print("$name has $age");
+  }
+
+  // for each
+  var collection = [1, 2, 3];
+  collection.forEach(print);
+}
+
+class Person {
+  final String name;
+  final int age;
+  final int document;
+
+  Person(this.name, this.age, this.document);
+}
