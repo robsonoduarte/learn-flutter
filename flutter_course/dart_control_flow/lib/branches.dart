c21a53d64da0d11f416cade63baa174e58173676
Robson Duarte <robson.o.d@gmail.com>
2023-12-02 00:24:41 -0300

    finish the dart controll flow chapter (#111)

--- flutter_course/dart_control_flow/lib/branches.dart
+++ flutter_course/dart_control_flow/lib/branches.dart
@@ -21,4 +21,19 @@ main() {
   if (pair case [int x, int y]) {
     print('Was coordinate array $x,$y');
   }
+
+  // Switch
+  // https://dart.dev/language/branches#switch-statements
+  switch (grade) {
+    case 10:
+    case 8:
+    case 9:
+      print('approved your grade is $grade');
+    case 7:
+    case 6:
+    case 5:
+      print('recuperation because your grade is $grade');
+    default:
+      print("you are reproved your grade is $grade");
+  }
 }
