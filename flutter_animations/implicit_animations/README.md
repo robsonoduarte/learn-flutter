e1d336bbff3f3548af7257c6bb7fbb9b89f64f78
Robson Duarte <robson.o.d@gmail.com>
2024-10-18 00:05:00 -0300

    flutter animations - implicit animations (#124)

---
+++ flutter_animations/implicit_animations/README.md
@@ -0,0 +1,6 @@
+## implicit animations
+
+
+This project is based in this [flutter codelabs](https://docs.flutter.dev/codelabs/implicit-animations).
+
+### The topics are:
---
+++ flutter_animations/implicit_animations/lib/fade_in.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+const owlUrl =
+    'https://raw.githubusercontent.com/flutter/website/main/src/content/assets/images/docs/owl.jpg';
+
+class FadeIn extends StatefulWidget {
+  const FadeIn({super.key});
+  @override
+  State<FadeIn> createState() => _FadeInState();
+}
+
+class _FadeInState extends State<FadeIn> {
+  double opacity = 0;
+  bool show = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FadeIn')),
+      body: ListView(children: <Widget>[
+        Image.network(owlUrl),
+        TextButton(
+          child: Text(
+            show ? 'Show Details' : 'Hide Details',
+            style: const TextStyle(color: Colors.blueAccent),
+          ),
+          onPressed: () => setState(() {
+            if (show) {
+              show = false;
+              opacity = 1;
+            } else {
+              show = true;
+              opacity = 0;
+            }
+          }),
+        ),
+        AnimatedOpacity(
+          duration: const Duration(seconds: 2),
+          opacity: opacity,
+          child: const Column(
+            children: [
+              Text('Type: Owl'),
+              Text('Age: 39'),
+              Text('Employment: None'),
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
