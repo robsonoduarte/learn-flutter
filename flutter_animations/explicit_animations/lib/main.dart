27db79b69ae3a3574744ba865c29962f671a7ef9
robsonoduarte <robson.o.d@gmail.com>
2024-12-04 00:24:38 -0300

    implicit animatinos - first implementation

--- flutter_animations/explicit_animations/lib/main.dart
+++ flutter_animations/explicit_animations/lib/main.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LogoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LogoApp extends StatefulWidget {
+  const LogoApp({super.key});
+
+  @override
+  State<LogoApp> createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(
+        () {
+          setState(() {});
+        },
+      );
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Explicit Animations',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: Text("Explicit Animations"),
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            height: animation.value,
+            width: animation.value,
+            child: const FlutterLogo(),
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void deactivate() {
+    controller.dispose();
+    super.dispose();
   }
 }
