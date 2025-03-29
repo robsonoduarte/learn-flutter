ddb73d2d4756c013fe033df4494ec69ad6b9174a
Robson Duarte <robson.o.d@gmail.com>
2025-03-29 01:34:22 -0300

    implicit animations - rendering animations example 1 (#131)

---
+++ flutter_animations/explicit_animations/lib/animation_controller_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AnimationControllerScreen extends StatefulWidget {
+  const AnimationControllerScreen({super.key});
+
+  @override
+  State<AnimationControllerScreen> createState() =>
+      _AnimationControllerScreenState();
+}
+
+class _AnimationControllerScreenState extends State<AnimationControllerScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(
+        () {
+          setState(() {});
+        },
+      );
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animation Controller"),
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: animation.value,
+          width: animation.value,
+          child: const FlutterLogo(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void deactivate() {
+    controller.dispose();
+    super.dispose();
+  }
+}
--- flutter_animations/explicit_animations/lib/main.dart
+++ flutter_animations/explicit_animations/lib/main.dart
@@ -1,23 +1,32 @@
+import 'package:explicit_animations/animation_controller_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: ExplicitAnimation()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExplicitAnimation extends StatefulWidget {
+  const ExplicitAnimation({super.key});
 
   @override
+  State<ExplicitAnimation> createState() => _ExplicitAnimationState();
+}
+
+class _ExplicitAnimationState extends State<ExplicitAnimation> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Explicit Animations',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Explicit Animations"),
-        ),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AnimationControllerScreen(),
+                ),
+              );
+            },
+            child: const Text("Animation Controller")),
       ),
     );
   }
