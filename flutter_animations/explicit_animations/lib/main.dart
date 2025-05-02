69933b13ece281f87b249382332dc25d5bef8387
Robson Duarte <robson.o.d@gmail.com>
2025-05-01 23:22:50 -0300

    flutter animations - explicit animations status listener (#143)

--- flutter_animations/explicit_animations/lib/main.dart
+++ flutter_animations/explicit_animations/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:explicit_animations/animated_widget_screen.dart';
-import 'package:explicit_animations/animation_controller_screen.dart';
+import 'package:explicit_animations/screen/animated_widget_screen.dart';
+import 'package:explicit_animations/screen/animation_controller_screen.dart';
+import 'package:explicit_animations/screen/status_listener_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,6 +29,10 @@ class _ExplicitAnimationState extends State<ExplicitAnimation> {
             ElevatedButton(
               child: const Text("Animated Widget"),
               onPressed: () => push(const AnimatedWidgetScreen()),
+            ),
+            ElevatedButton(
+              child: const Text("Status Listener"),
+              onPressed: () => push(const StatusListenerScreen()),
             ),
           ],
         ),
